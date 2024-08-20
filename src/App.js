5cb2cd35b221592f8a6bff4e1bfa3dd6d9aeb854
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-08-19 21:37:59 -0300

    Ultilizando Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Meu ae karalho App</h1>
-    </div>
-  );
+
+  const nome = "Maria"
+
+return (
+  <div className="App">
+    <SayMyName nome="Udiel"/>
+    <SayMyName nome="João"/>
+    <SayMyName nome={nome}/>
+    <Pessoa nome="Udiel" idade="17" profissão="programador" foto="https://ferramentasinteligentes.com.br/wp-content/uploads/2015/06/water-209901_1280.jpg" />
+    
+  </div>
+);
 }
 
 export default App;
---
+++ src/components/Frase.js
@@ -0,0 +1,8 @@
+function Frase(){
+    return(
+        <div>
+            <p>Este é meu primeiro componente frase</p>
+        </div>
+    )
+}
+export default Frase
