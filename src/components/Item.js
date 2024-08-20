201322f45130fed5769647641eb0b99bcfc3e0e9
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-08-20 08:52:46 -0300

    propType

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,21 @@
-function Item(props) {
+import { PropTypes } from "prop-types";
+
+function Item({marca, ano_lancamento}) {
     return(
         <>
-            <li><h2>{props.marca}</h2></li>
+            <li><h2>{marca}</h2> - {ano_lancamento}</li>
             <h2>opa</h2>
         </>
     )
+}
+Item.propTypes = {
+    marca: PropTypes.string,
+    ano_lancamento: PropTypes.number,
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca filha da puta',
+    ano_lancamento: 0,
 }
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -3,10 +3,12 @@ import Item from "./Item"
 function List() {
     return (
         <>
-            <h1>Minha lista</h1>
+        <h1>Minha lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Ford" ano_lancamento={1964}/>
+                <Item marca="Fiat" ano_lancamento={400}/>
+                <Item/>
             </ul>
         </>
     )
