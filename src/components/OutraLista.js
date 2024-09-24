868c98a6f76365dad02e549aaf88fb48f2539a34
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-09-23 21:08:38 -0300

    Renderização de Listas

---
+++ src/components/OutraLista.js
@@ -0,0 +1,17 @@
+function OutraLista({itens}) {
+    return (
+        <>
+            <h3>Lista de Coisas Boas:</h3>
+            {itens.length > 0 ? (
+            
+            itens.map((item, index) => (
+                <p key={index}>{item}</p>
+            ))) : (
+                <p>Nao há itens na lista</p>
+            ) 
+            }
+        </>
+    )
+}
+
+export default OutraLista
